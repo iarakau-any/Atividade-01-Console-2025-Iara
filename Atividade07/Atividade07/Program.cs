dd17512dbd250d273965ff73de6b27baf1bd1a52
Iara <kauanysoneghetti@gmail.com>
2025-09-09 08:52:24 -0300

    Atividade 07 finalizada

---
+++ Atividade07/Atividade07/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Digite um numero");
+int numero = int.Parse(Console.ReadLine());
+if (numero > 10)
+{
+    Console.WriteLine("Esse numero é maior que 10!");
+}
+if (numero < 10)
+{
+    Console.WriteLine("Esse numero não é maior que 10!");
+}
+
+
+
+
