779589d851661654c993fa5f115ec8dfc6e6f25b
Iara <kauanysoneghetti@gmail.com>
2025-09-09 10:34:05 -0300

    Atividade 09 realizada

---
+++ Atividade09/Atividade09/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+Console.WriteLine("=== CALCULADORA DE PREÇO DE MAÇÃS ===");
+Console.WriteLine("Preço: R$1,30 cada (menos de 12 unidades)");
+Console.WriteLine("Preço: R$1,00 cada (12 unidades ou mais)");
+Console.WriteLine("--------------------------------------");
+Console.Write("Digite o número de maçãs compradas: ");
+int numeroMacas = int.Parse(Console.ReadLine());
+double custoTotal;
+  if (numeroMacas < 12)
+       {
+            custoTotal = numeroMacas * 1.30;
+       }
+  else
+       {
+            custoTotal = numeroMacas * 1.00;
+       }
+Console.WriteLine("--------------------------------------");
+Console.WriteLine($"Número de maçãs: {numeroMacas}");
+Console.WriteLine($"Preço unitário: R${(numeroMacas < 12 ? "1,30" : "1,00")}");
+Console.WriteLine($"Custo total: R${custoTotal:F2}");
+
