df6abf24f41cfb13df06843de1284659c0a71ffa
Iara <kauanysoneghetti@gmail.com>
2025-09-09 11:09:37 -0300

    Atividade 12 finalizada

---
+++ atividade12/atividade12/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("=== MAIOR VALOR ===");
+Console.Write("Digite o primeiro valor: ");
+double valor1 = double.Parse(Console.ReadLine());
+
+Console.Write("Digite o segundo valor: ");
+double valor2 = double.Parse(Console.ReadLine());
+
+if (valor1 == valor2)
+{
+    Console.WriteLine("Erro: Os valores não podem ser iguais!");
+    return;
+}
+if (valor1 > valor2)
+{
+    Console.WriteLine($"O maior valor é: {valor1}");
+}
+else
+{
+    Console.WriteLine($"O maior valor é: {valor2}");
+}
