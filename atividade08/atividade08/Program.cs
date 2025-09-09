727722afd4f114834e23c9c317588ce07e29d62f
Iara <kauanysoneghetti@gmail.com>
2025-09-09 09:02:30 -0300

    Atividade 08 finalizada

---
+++ atividade08/atividade08/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Digite um valor");
+int numero = int.Parse(Console.ReadLine());
+if (numero == 0)
+{
+    Console.WriteLine("Não permitimos o valor 0");
+}
+
+if (numero > 0)
+{
+    Console.WriteLine("Esse valor é positivo");
+}
+
+if (numero <0)
+{
+    Console.WriteLine("Esse valor é negativo");
+}
