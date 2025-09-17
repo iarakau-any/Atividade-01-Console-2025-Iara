e3212ec7e39fc22cd194650eb7cea65bd5ae2632
Iara <kauanysoneghetti@gmail.com>
2025-09-17 10:17:47 -0300

    Atividade 16 concluida

---
+++ Atividade16/Atividade16/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Digite um numero:");
+ int num = int.Parse(Console.ReadLine());
+int fatorial = 1;
+for (int contador = 1; contador <= num; contador++)
+{
+
+    if (num == 0)
+    {
+        Console.WriteLine("0 = 1, por definição");
+    }
+    else
+    {
+        fatorial *= contador;
+    }
+
+}
+if(num < 0) 
+{
+    Console.WriteLine("Negativo = não existe!");
+}
+else
+{
+    Console.WriteLine(fatorial);
+}
