ee7563b66fe79dbdbcdeccf2e0d083b819f50f7b
Iara <kauanysoneghetti@gmail.com>
2025-09-17 08:20:40 -0300

    Atividade 14 concluida

---
+++ Atividade14/Atividade14/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+int num;
+Console.WriteLine("Digite um valor:");
+num = int.Parse(Console.ReadLine());
+for (int contador = 2; contador < num; contador++)
+{
+    Console.WriteLine(contador);
+}
+
+
