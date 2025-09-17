ea4cd7af77f529afbd0e03cf6880d59eb0b98bfb
Iara <kauanysoneghetti@gmail.com>
2025-09-17 08:49:01 -0300

    Atividade 15 concluida

---
+++ Atividade15/Atividade15/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.ConstrainedExecution;
+
+int resultado;
+//int = resultado;
+for(int contador = 1000; contador <= 2000; contador++)
+{
+    resultado = contador % 11;
+    if (resultado == 5)
+    {
+        Console.WriteLine(contador);
+    }
+}
